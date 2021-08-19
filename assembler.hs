5a12458d5a9ae5f4e90d070e2cf3f605a6726a3f
Zak-Kent <zak.kent@gmail.com>
2021-08-18 17:23:58 -0700

    Add lookup maps for C instructions

--- assembler.hs
+++ assembler.hs
@@ -97,6 +97,30 @@ cInstructionP = do
   jump <- option Nothing (fmap Just jumpP)
   return (CInstruction dest comp jump)
 
+destIns :: (M.Map String String)
+destIns = M.fromList [("M", "001"), ("D", "010"), ("MD", "011"), ("A", "100"),
+                      ("AM", "101"), ("AD", "110"), ("AMD", "111")]
+
+type CompInstruction = (Cbits, Abit)
+type Cbits = String
+type Abit = String
+
+compIns :: (M.Map String CompInstruction)
+compIns = M.fromList [("0", ("101010", "0")), ("1", ("111111", "0")), ("-1", ("111010", "0")),
+                      ("D", ("001100", "0")), ("A", ("110000", "0")), ("!D", ("001101", "0")),
+                      ("!A", ("110001", "0")), ("-D", ("001111", "0")), ("-A", ("110011", "0")),
+                      ("D+1", ("011111", "0")), ("A+1", ("110111", "0")), ("D-1", ("001110", "0")),
+                      ("A-1", ("110010", "0")), ("D+A", ("000010", "0")), ("D-A", ("010011", "0")),
+                      ("A-D", ("000111", "0")), ("D&A", ("000000", "0")), ("D|A", ("010101", "0")),
+                      ("M", ("110000", "1")), ("!M", ("110001", "1")), ("-M", ("110011", "1")),
+                      ("M+1", ("110111", "1")), ("M-1", ("110010", "1")), ("D+M", ("000010", "1")),
+                      ("D-M", ("010011", "1")), ("M-D", ("000111", "1")), ("D&M", ("000000", "1")),
+                      ("D|M", ("010101", "1"))]
+
+jumpIns :: (M.Map String String)
+jumpIns = M.fromList [("JGT", "001"), ("JEQ", "010"), ("JGE", "011"), ("JLT", "100"), ("JNE", "101"),
+                      ("JLE", "110"), ("JMP", "111")]
+
 translateC :: String -> Maybe String
 translateC line = Just "C instruction"
 
