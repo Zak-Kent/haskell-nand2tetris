47138c43a5755d10cbff352f605b51dc8d2a8c24
Zak-Kent <zak.kent@gmail.com>
2021-09-01 21:12:48 -0700

    Add line parser

--- vmTranslator.hs
+++ vmTranslator.hs
@@ -1,6 +1,6 @@
 import Text.ParserCombinators.ReadP
 import Control.Applicative hiding (optional)
-
+import Data.Char (isSpace)
 
 -- TODO: will need to break this out into seperate files later on
 
@@ -13,14 +13,57 @@ data MemSegment = Local | Argument | This | That | Constant | Static | Temp | Po
                 deriving (Show)
 type Index = Integer
 
+data Line = Line { command :: Command,
+                   memSegment :: MemSegment,
+                   index :: Index
+                 } deriving (Show)
 
-pushP :: ReadP String
-pushP = string "push"
+pushP :: ReadP Command
+pushP = do
+  string "push"
+  return Push
 
-popP :: ReadP String
-popP = string "pop"
+popP :: ReadP Command
+popP = do
+  string "pop"
+  return Pop
 
-arithmeticP :: ReadP String
-arithmeticP = string "add" <|> string "sub" <|> string "neg" <|> string "eq" <|>
-              string "gt" <|> string "lt" <|> string "and" <|> string "or" <|>
-              string "not"
+arithmeticP :: ReadP Command
+arithmeticP = do
+  art <- string "add" <|> string "sub" <|> string "neg" <|> string "eq" <|>
+         string "gt" <|> string "lt" <|> string "and" <|> string "or" <|>
+         string "not"
+  return $ Arithmetic art
+
+toMemSegment :: String -> MemSegment
+toMemSegment seg = case seg of
+                     "local" -> Local
+                     "argument" -> Argument
+                     "this" -> This
+                     "that" -> That
+                     "constant" -> Constant
+                     "static" -> Static
+                     "temp" -> Temp
+                     "pointer" -> Pointer
+
+memSegmentP :: ReadP MemSegment
+memSegmentP = do
+  seg <- string "local" <|> string "argument" <|> string "this" <|>
+         string "that" <|> string "constant" <|> string "static" <|>
+         string "temp" <|> string "pointer"
+  return $ toMemSegment seg
+
+isDigit :: Char -> Bool
+isDigit c = c >= '0' && c <= '9'
+
+indexP :: ReadP Index
+indexP = fmap read $ munch1 isDigit
+
+lineP :: ReadP Line
+lineP = do
+  cmd <- popP <|> pushP <|> arithmeticP
+  satisfy isSpace
+  memSeg <- memSegmentP
+  satisfy isSpace
+  idx <- indexP
+  return (Line cmd memSeg idx)
