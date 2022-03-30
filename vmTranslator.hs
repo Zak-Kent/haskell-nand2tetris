b3519e89b57c172afc744fa041d7245e52630dc3
Zak-Kent <zak.kent@gmail.com>
2022-03-30 11:41:02 -0700

    Update VM translator to handle directory of .vm files
    This commit adds directory support to the VMTranslator. If invoked with
    a path to a dir containing multiple .vm files the translator will
    translate all of the files and produce a single <dir-name>.asm file
    as a result.

--- vmTranslator.hs
+++ vmTranslator.hs
@@ -1,26 +1,38 @@
 import System.IO
 import System.Environment
-import System.FilePath.Posix(replaceExtensions)
+import System.FilePath.Posix(replaceExtensions, takeBaseName, takeExtensions)
+import System.Directory
 import VMParser
 import VMWriter
 
-getDaArgs :: [String] -> String
-getDaArgs [] = "da fuq"
-getDaArgs (x:_) = x
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let srcFile = getDaArgs args
+  -- No error handling for missing file args or checks to prevent head on []
+  -- Assumes the Coursera grader is calling with correct arguments
+  s <- getArgs
+  let src = head s
+      readAndTranslate f = do
+        contents <- fmap lines $ readFile f
+        return $ translateFile $ parseLines contents
+      callSysInit = translateFile $ parseLines ["call Sys.init 0"]
+      setSPto256 = [(unlines ["@256", "D=A", "@SP", "M=D"])]
+      isVmFile = (\f -> ".vm" == (takeExtensions f))
+  isDir <- doesDirectoryExist src
 
-  if srcFile == "da fuq" then do
-    putStrLn "no src file arg"
+  if isDir == True then do
+    -- TODO takeBaseName doesn't work with a trailing slash, make sure this isn't an issue
+    let dst = src ++ "/" ++ (takeBaseName src) ++ ".asm"
+    files <- listDirectory src
+    output <- mapM readAndTranslate
+      $ map (\f -> src ++ "/" ++ f)
+      $ filter isVmFile files -- needed to filter out the non .vm files
+    outFile <- openFile dst WriteMode
+    hPutStr outFile $ concat $  setSPto256 ++ [callSysInit] ++ output
+    hClose outFile
   else do
-    let dstFile = replaceExtensions srcFile "asm"
-
-    contents <- fmap lines $ readFile srcFile
-    let output = translateFile $ parseLines contents
-
-    outFile <- openFile dstFile WriteMode
-    hPutStr outFile output
+    let dst = replaceExtensions src "asm"
+    output <- readAndTranslate src
+    outFile <- openFile dst WriteMode
+    hPutStr outFile $ output ++ (unlines endProg)
     hClose outFile
