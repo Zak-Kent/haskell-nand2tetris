8aa5f5272bde677fbac2ed2fea29975b1bedde86
Zak-Kent <zak.kent@gmail.com>
2022-04-12 12:06:42 -0700

    Add helper for string literal parsers

--- JackCompiler/parser.hs
+++ JackCompiler/parser.hs
@@ -2,14 +2,15 @@ import AST
 import Text.ParserCombinators.ReadP
 import Control.Applicative hiding (optional)
 
+chooseLit :: [String] -> ReadP String
+chooseLit xs = choice [string x | x <- xs]
+
 keywordP :: ReadP Keyword
 keywordP = do
-  kw <- string "class" <|> string "constructor" <|> string "function"
-        <|> string "method" <|> string "field" <|> string "static"
-        <|> string "var" <|> string "int" <|> string "char" <|> string "boolean"
-        <|> string "void" <|> string "true" <|> string "false" <|> string "null"
-        <|> string "this" <|> string "let" <|> string "do" <|> string "if"
-        <|> string "else" <|> string "while" <|> string "return"
+  kw <- chooseLit ["class", "constructor", "function", "method", "field",
+                   "static", "var", "int", "char", "boolean", "void", "true",
+                   "false", "null", "this", "let", "do", "if", "else", "while",
+                   "return"]
   return (Keyword kw)
 
 main :: IO ()
