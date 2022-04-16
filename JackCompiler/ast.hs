aa5b391ba1b49841ab5412709b02119b67ea5eed
Zak-Kent <zak.kent@gmail.com>
2022-04-16 13:27:52 -0700

    Add parser support for Jack terms
    This commit adjusts the types the parser produces. It moves terminal
    types which are only seen in Jack 'terms' under the Term type. This is
    needed so the types line up when combining the individual parsers for
    each terminal element. It also includes a type and parser for expressions
    which are included in Jack 'terms'.

--- JackCompiler/ast.hs
+++ JackCompiler/ast.hs
@@ -3,11 +3,17 @@ module AST where
 -- Jack terminals
 data Keyword = Keyword String deriving (Show)
 data Symbol = Symbol String deriving (Show)
-data IntegerConstant = IntegerConstant Int deriving (Show)
-data StringConstant = StringConstant String deriving (Show)
 data Identifier = Identifier String deriving (Show)
-
--- Jack expressions
-data KeywordConstant = KeywordConstant String deriving (Show)
-data UnaryOp = UnaryOp String deriving (Show)
 data Op = Op String deriving (Show)
+
+-- expression: term (op term)*
+data Expr = Expr Term [(Op, Term)] deriving (Show)
+
+data Term = IntegerConstant Int
+  | StringConstant String
+  | KeywordConstant String
+  | VarName Identifier
+  | UnaryOp String Term
+  | VarNameExpr Identifier Symbol Expr Symbol
+  | ParenExpr Symbol Expr Symbol
+  deriving (Show)
--- JackCompiler/parser.hs
+++ JackCompiler/parser.hs
@@ -4,6 +4,7 @@ import qualified Text.Parsec as Ps
 chooseLit :: [String] -> Ps.Parsec String () String
 chooseLit xs = Ps.choice [Ps.try $ Ps.string x | x <- xs]
 
+-- Terminal element parsers
 keywordP :: Ps.Parsec String () Keyword
 keywordP = do
   kw <- chooseLit ["class", "constructor", "function", "method", "field",
@@ -11,11 +12,6 @@ keywordP = do
                    "false", "null", "this", "let", "do", "if", "else", "while",
                    "return"]
   return (Keyword kw)
-
-keywordConstantP :: Ps.Parsec String () KeywordConstant
-keywordConstantP = do
-  kwc <- chooseLit ["true", "false", "null", "this"]
-  return (KeywordConstant kwc)
 
 symbolP :: Ps.Parsec String () Symbol
 symbolP = do
@@ -28,28 +24,62 @@ opP = do
   op <- chooseLit ["+", "-", "*", "/", "&", "|", "<", ">", "="]
   return (Op op)
 
-unaryOpP :: Ps.Parsec String () UnaryOp
-unaryOpP = do
-  uop <- chooseLit ["-", "~"]
-  return (UnaryOp uop)
-
-integerConstantP :: Ps.Parsec String () IntegerConstant
-integerConstantP = do
-  -- use of read safe because only ascii digits 0-9 make it past the parser
-  i <- fmap read $ Ps.many1 Ps.digit
-  return (IntegerConstant i)
-
-stringConstantP :: Ps.Parsec String () StringConstant
-stringConstantP = do
-  s <- Ps.between (Ps.char '"') (Ps.char '"') (Ps.many1 $ Ps.noneOf ['"', '\n'])
-  return (StringConstant s)
-
 identifierP :: Ps.Parsec String () Identifier
 identifierP = do
   {- a seq of letters, digits, and '_' not starting with a digit -}
   x <- Ps.noneOf ['0'..'9']
   xs <- Ps.many $ Ps.choice [Ps.digit, Ps.letter, Ps.char '_']
   return (Identifier $ [x] ++ xs)
+
+-- Expr parsers
+opTermP :: Ps.Parsec String () (Op, Term)
+opTermP = do
+  Ps.spaces
+  op <- opP
+  Ps.spaces
+  t <- termP
+  return (op, t)
+
+exprP :: Ps.Parsec String () Expr
+exprP = do
+  t <- termP
+  Ps.spaces
+  opTerms <- Ps.many opTermP
+  return (Expr t opTerms)
+
+-- Term parsers
+keywordConstantP :: Ps.Parsec String () Term
+keywordConstantP = do
+  kwc <- chooseLit ["true", "false", "null", "this"]
+  return (KeywordConstant kwc)
+
+unaryOpP :: Ps.Parsec String () Term
+unaryOpP = do
+  uop <- chooseLit ["-", "~"]
+  t <- termP
+  return (UnaryOp uop t)
+
+integerConstantP :: Ps.Parsec String () Term
+integerConstantP = do
+  -- use of read safe because only ascii digits 0-9 make it past the parser
+  i <- fmap read $ Ps.many1 Ps.digit
+  return (IntegerConstant i)
+
+stringConstantP :: Ps.Parsec String () Term
+stringConstantP = do
+  s <- Ps.between (Ps.char '"') (Ps.char '"') (Ps.many1 $ Ps.noneOf ['"', '\n'])
+  return (StringConstant s)
+
+varNameP :: Ps.Parsec String () Term
+varNameP = do
+  vn <- identifierP
+  return (VarName vn)
+
+termP :: Ps.Parsec String () Term
+termP = do
+  t <- Ps.choice $ map Ps.try [integerConstantP, stringConstantP,
+                               keywordConstantP, unaryOpP, varNameP]
+  return t
 
 main :: IO ()
 main = do
